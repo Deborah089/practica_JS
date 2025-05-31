14d3ffd1f1bb3de20154aee13a009899d0cf9edc
Déborah089 <jade89_06@hotmail.com>
2025-05-30 22:20:31 -0300

    prueba git

---
+++ index.js
@@ -0,0 +1,22 @@
+//1.-
+alert("HOLA MUNDO")
+//2.-
+document.write("HOLA MUNDO")
+//3.-
+console.log (3+5);
+//4.-
+var nombre = prompt ("Ingrese su nombre", "")
+document.write ("Hola " + nombre)
+//5.-
+
+var nota = 7;
+document.write("El examen ha resultado: ");
+//condicion
+if (nota < 5) {
+    //accion A
+    calificacion = ("suspendido");
+}else{
+    //accion B
+    calificacion = "aprobado";
+}
+document.write("Estoy,calificacion");
